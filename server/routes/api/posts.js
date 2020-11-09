75b10f1161414f335eeca9d1594daf506d367a15
Cody Patterson <codytpatterson@gmail.com>
2020-11-09 09:58:20 -0800

    created db and updated mongoclient constructor

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
 const router = express.Router();
 
 // Get Post
-router.get('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    res.send(await posts.find('{}').toArray());
+router.get("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find("{}").toArray());
 });
 
 // Add Post
-router.post('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    await posts.insertOne({
-        text: req.body.text,
-        createdAt: new Date()
-    });
-    res.status(201).send();
+router.post("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
+  res.status(201).send();
 });
 
 // Delete Post
-router.delete('/:id', async (req, res) => {
-    const posts = await loadPostsCollection();
-    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
-    res.status(200).send();
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
 });
 
 async function loadPostsCollection() {
-    const client = await mongodb.MongoClient.connect
-    ('mongodb://cody:abc123@localhost:27017/vue_express', {
-        useNewUrlParser: true
-    });
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://cody:n3xtg3n@cluster0.azniz.mongodb.net/type_wars?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
 
-    return client.db('vue_express').collection('posts');
+  return client.db("type_wars").collection("posts");
 }
 
 module.exports = router;
