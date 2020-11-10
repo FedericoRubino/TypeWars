a7d73861d1f6ca60989be4befd23d3714357cdf9
Cody Patterson <codytpatterson@gmail.com>
2020-11-09 16:28:28 -0800

    updated getPosts function

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,37 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'api/posts/';
+const url = "api/posts/";
 
 class PostService {
-    // Get Posts
-    static getPosts() {
-        return new Promise((resolve, reject) => {
-            try {
-                const res = axios.get(url);
-                const data = res.data;
-                resolve(
-                    data.map(post => ({
-                        ...post,
-                        createdAt: new Date(post.createdAt)
-                    }))
-                );
-            } catch(err) {
-                reject(err);
-            }
+  // Get Posts
+  static getPosts() {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url)
+        .then((res) => {
+          const data = res.data;
+          resolve(
+            data.map((post) => ({
+              ...post,
+              createdAt: new Date(post.createdAt),
+            }))
+          );
+        })
+        .catch((err) => {
+          reject(err);
         });
-    }
+    });
+  }
 
-    // Create Post
-    static insertPost(text) {
-        return axios.post( url, {
-            text
-        });
-    }
+  // Create Post
+  static insertPost(text) {
+    return axios.post(url, {
+      text,
+    });
+  }
 
-    // Delete Post
-    static deletePost(id) {
-        return axios.delete(`${url}${id}`);
-    }
+  // Delete Post
+  static deletePost(id) {
+    return axios.delete(`${url}${id}`);
+  }
 }
 
 export default PostService;
